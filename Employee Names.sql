deb1124625a5e733a4f7f3a7a8de03b2c462da99
ashraf <ashraf.istvn@gmail.com>
2019-04-17 12:02:05 +0700

    #Beginner level problem solved all

---
+++ Employee Names.sql
@@ -0,0 +1,21 @@
+/*
+    Employee Names
+    Write a query that prints a list of employee names 
+    (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+
+    Sample Output
+
+    Angela
+    Bonnie
+    Frank
+    Joe
+    Kimberly
+    Lisa
+    Michael
+    Patrick
+    Rose
+    Todd
+*/
+SELECT name FROM employee
+ORDER BY name ASC;
