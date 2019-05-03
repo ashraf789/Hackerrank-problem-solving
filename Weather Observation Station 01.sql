1a5be9eb9a5c0b924680d929edf89f0eb54765ed
ashraf <ashraf.istvn@gmail.com>
2019-05-03 08:33:17 +0700

    update repository

--- Weather Observation Station 01.sql
+++
@@ -1,7 +0,0 @@
-/*
-	Weather Observation Station 1
-	Query a list of CITY and STATE from the STATION table.
-*/
-select city, state from station;
-
-
--- Weather Observation Station 03.sql
+++
@@ -1,7 +0,0 @@
-/*
- Weather Observation Station 03
- Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
- 
-*/
-
-select DISTINCT city from station where id%2 = 0;
