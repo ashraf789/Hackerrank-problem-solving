a44ee2e619a299167668b33c6c046695f340740b
ashraf789 <syedashrafullah15@gmail.com>
2020-05-22 22:07:55 +0700

    [add] triangle 1 and 2

---
+++ SQL-Problems/Draw The Triangle 1.sql
@@ -0,0 +1,8 @@
+-- The repeat(str, count) function will repeat a string up to the count value
+-- for example repeat('*', 4) -> this will print ****
+-- The information_schema.tables has 50+ tables that means we can print 50+ line
+-- Each line we will repeat a string up to the @n value and before start we will re assign the @n variable by @n-1 
+ 
+
+SET @n := 21;
+SELECT repeat('* ', @n := @n-1) FROM information_schema.tables limit 20;
